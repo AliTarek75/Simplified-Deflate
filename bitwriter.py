2465a3567ae9076b58c86055cc3a0f6ba0666704
Abdelrazek0 <ahmedmabdelrazek1@gmail.com>
2026-05-09 14:24:23 +0300

    readded comments

--- bitwriter.py
+++ bitwriter.py
@@ -1,6 +1,7 @@
 import math
 
-def compressFile(stage2):
+# combine header in one long string and create the binary file
+def compressFile(stage2, filename):
     payload,lit_codes, dist_codes = pipeline(stage2)
 
     LIT_BW, DIST_BW = calculateBW(lit_codes,dist_codes)
@@ -8,6 +9,8 @@ def compressFile(stage2):
     distBits = distPadding(DIST_BW, dist_codes)
 
     headerStr = format(LIT_BW, '04b') + format(DIST_BW, '04b') + litBits + distBits
+
+    # pad total header size to be multiple of 8 so file size be integer number of bytes
     while ((len(headerStr) % 8) != 0):
         headerStr += '0'
     
@@ -15,12 +18,13 @@ def compressFile(stage2):
     for i in range(0, len(headerStr), 8):
         headerBytes.append(int(headerStr[i:i+8], 2))
     
-    file = open("test.bin", 'wb')
+    file = open(f"{filename}.bin", 'wb')
     file.write(headerBytes + payload)
 
-
+# calculate min bits for max length/distance to be written in binary
 def calculateBW(lit_codes,dist_codes):
     max_length = 0
+    #value is tuple of size 2 where the first element is huffman code in decimal and the second element is the length
     for value in lit_codes.values():
         if (value[1] > max_length):
             max_length = value[1]
@@ -34,16 +38,18 @@ def calculateBW(lit_codes,dist_codes):
     DIST_BW = 0
     if (max_length > 0):
         LIT_BW = math.floor(math.log2(max_length))+1
+
     if (max_distance > 0):
         DIST_BW = math.floor(math.log2(max_distance))+1
-    return LIT_BW, DIST_BW
 
+    return LIT_BW, DIST_BW
 
 def litPadding(LIT_BW,lit_codes):
     litBits = ""
 
     if (LIT_BW > 0):
         for i in range(286):
+            # pad the symbol if exist and if it doesn't exist pad 0 because all symbols must be in the header for easily decoding
             if (i in lit_codes):
                 litBits += format(lit_codes[i][1], f'0{LIT_BW}b')
             else:
--- lz77.py
+++ lz77.py
@@ -1,48 +1,58 @@
+# max distance we can look for a match
 WINDOW_SIZE = 32768
+
 MIN_MATCH = 3
 MAX_MATCH = 258
+
+# max number of candidates we can check 
 MAX_CANDIDATES = 64
 
-data = open('data.txt', 'rb').read()
-
+# tokenProducer is made to search for pattern and return list of tokens where literals are int and matches are tuple (length, distance)
 def tokenProducer(data):
     tokens = []
+    # dict to append all the 3 bytes sequences in the input so we can return back faster at specific pos and check the match
     table = {}
     i = 0
 
     while(i < len(data)):
+        # if fewer than 3 bytes remain so no match is possible
         if ((len(data) - i) < 3):
             tokens.append(data[i])
             i+=1
             continue
 
+        # get next 3 bytes together to check if there is a match or no
         current = data[i:i+3]
         if (current in table):
             length = 0
-            final_pos = 0
+            distance = 0
+            # we check the last 64 position where the current match happened to calculate the longest length
+            # if lengths are equal take nearest pos that's why we reversed the last 64 pos to start from newest
             for pos in reversed(table[current][-MAX_CANDIDATES:]):
                 matchIdx = i
                 matchPos = pos
 
+                # ignore the match if it's distance is larger than max distance
                 if ((i - pos) > WINDOW_SIZE):
                     continue
-
+                # search for the end of the match and update length
                 while(((matchIdx - i) < MAX_MATCH) and (matchIdx < len(data)) and (data[matchPos] == data[matchIdx])):
                     matchPos+=1
                     matchIdx+=1
 
                 if (length < (matchIdx - i)):
                     length = matchIdx - i
-                    final_pos = i - pos
+                    distance = i - pos
             
+            # if match is valid then append it in the table and update all other sequences and jump for the next byte outside the match
             if (length >= MIN_MATCH):
-                tokens.append((length,final_pos))
+                tokens.append((length,distance))
 
                 for x in range(i, i+length):
                     if ((x + 3) <= len(data)):
                         table.setdefault(data[x:x+3], []).append(x)
                 i += length
-
+            #if match is less than min_match then append literal
             else:
                 tokens.append(data[i])
                 i+=1
@@ -51,4 +61,5 @@ def tokenProducer(data):
             table[current] = [i]
             tokens.append(data[i])
             i+=1
+
     return tokens
